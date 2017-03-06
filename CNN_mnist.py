e364bceb912090f0c5697953ead02a51b7731b79
JiaRenChang <followwar@gmail.com>
2017-03-07 01:47:25 +0800

    add data

--- CNN_mnist.py
+++ CNN_mnist.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 import tensorflow as tf
 
 #use others' data loader
-from tensorflow.examples.tutorials.mnist import input_data
+import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # define functions
